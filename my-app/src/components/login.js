ae62992069139f047ddad4d728fde1b66515bf2a
Disitha Ranasinghe <120271466+Disitha395ra@users.noreply.github.com>
2024-11-26 09:21:42 +0000

    create login.js

--- my-app/src/components/login.js
+++ my-app/src/components/login.js
@@ -1,7 +1,14 @@
+import { useNavigate } from "react-router-dom"
+
 export default function login(){
+    const navigate = useNavigate();
+    const authenticate=()=>{
+        navigate('/dashboard')
+    }
     return(
         <div>
             <h1>Login Here</h1>
+            <button onClick={authenticate}>Click Me</button>
         </div>
     )
 }
