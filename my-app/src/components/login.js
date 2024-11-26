5aa332c5a705c7e6273e9b2f0d65c479d28a5176
Disitha Ranasinghe <120271466+Disitha395ra@users.noreply.github.com>
2024-11-26 09:51:48 +0000

    create form validations

--- my-app/src/components/login.js
+++ my-app/src/components/login.js
@@ -1,14 +1,54 @@
-import { useNavigate } from "react-router-dom"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import React from "react";
+
 
 export default function login(){
     const navigate = useNavigate();
-    const authenticate=()=>{
-        navigate('/dashboard')
+
+    const users=({
+        username:'admin',
+        password:'123'
+    });
+
+    const [error, seterror]=useState('')
+    const [user, setuser]=useState('')
+    
+    const authenticate=(e)=>{
+        e.preventDefault();
+        if(!username || !password){
+            seterror('Please provide all the fields')
+            return
+        }
+
+        if(e.password.length<8){
+            seterror('Password must contain 8 charcters')
+            return
+        }
+
+        if(users.username === e.username || users.password === e.password){
+            navigate('/dashboard')
+        }
     }
+
     return(
         <div>
             <h1>Login Here</h1>
-            <button onClick={authenticate}>Click Me</button>
+            <form onSubmit={authenticate}>
+
+                <input type="text"
+                name="username"
+                placeholder="Enter username"
+                value={users.username}/>
+
+                <input type="password"
+                name="password"
+                placeholder="Enter password"
+                value={users.password}/>
+
+
+                <button onClick={authenticate}>Click Me</button>
+            </form>
         </div>
     )
 }
